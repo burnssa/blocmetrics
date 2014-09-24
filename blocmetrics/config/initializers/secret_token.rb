9fbd778c32870519773eef3b7b8ef394795feff0
burnssa@gmail.com <burnssa@gmail.com>
2014-09-24 07:28:32 -0700

    basic chart added

---
+++ blocmetrics/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App::Application.config.secret_key_base = 'c38e55a3118f006c114634c2130596d2a43c691f1decd3e500150b44d04094a8ffa8831d2bf70995d601eba3ad7c7ac852d2153eab0a8adfdb5823ae255aed25'
