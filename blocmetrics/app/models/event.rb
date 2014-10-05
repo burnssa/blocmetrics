c0db9ea8717e1c7324130d7b7ca6e2c5c0e86565
burnssa@gmail.com <burnssa@gmail.com>
2014-10-04 19:55:34 -0700

    created visits hash

--- blocmetrics/app/models/event.rb
+++ blocmetrics/app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ActiveRecord::Base
 	belongs_to :tracked_app	
+
+	def visits
+		ips = Event.all.collect { |a| a.ip_address }.uniq
+		visits = []
+
+		ips.each do |v|
+			visits[v] = Event.where(ip_address: ips[v]).count
+			visits
+		end
+	end
+
 end
